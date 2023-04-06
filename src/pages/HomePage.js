672e8b33d06f0059794aca194699a06a543ef3aa
Minhtrong360 <107201955+Minhtrong360@users.noreply.github.com>
2023-04-06 14:28:27 +0700

    Update Homepages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,15 +4,14 @@ import StoriesWithGenres from "../features/story/StoriesWithGenres";
 
 function HomePage() {
   return (
-    <>
-      <StoriesWithGenres genres={"xuyên không"} />
-
-      <StoriesWithGenres genres={"thể thao"} />
+    <>  
       <StoriesWithGenres genres={"Adventure"} />
-      <StoriesWithGenres genres={"magic"} />
       <StoriesWithGenres genres={"comedy"} />
       <StoriesWithGenres genres={"drama"} />
       <StoriesWithGenres genres={"hành động"} />
+      <StoriesWithGenres genres={"magic"} />
+      <StoriesWithGenres genres={"thể thao"} />
+      <StoriesWithGenres genres={"xuyên không"} />
     </>
   );
 }
